b8abf3d0b9523ec7a4b3ab09a39ceb5201e9b92d
Kun Yang <kunyangnew@gmail.com>
2017-08-22 01:16:33 -0700

    update

--- application/views/mobile/resource_item.php
+++ application/views/mobile/resource_item.php
@@ -4,15 +4,15 @@ $(window).resize(function(){
 	var frame = $('.youtube-link');
 	frame.innerHeight(frame.innerWidth() *  o_height / o_width) ;
 });
-$(document).ready(function(){
+$(document).on("pageshow","#resource-item",function(){
 	var frame = $('.youtube-link');
 	o_width = parseInt(frame.attr('width'));
 	o_height = parseInt(frame.attr('height'));
 	frame.removeAttr('width').removeAttr('height').css('width', '100%').css('max-width', o_width + 'px');
 	frame.innerHeight(frame.innerWidth() *  o_height / o_width) ;
-});
+})
 </script>
-<div data-role="page" data-theme="d">
+<div data-role="page" id="resource-item" data-theme="d">
 	<?php $this->load->view('mobile/header', array('header_text' => $resource['subject'].' - Lily Fortune Club'));?>
 	<div data-role="main" class="ui-content">
 		<div class="resource-content">
