2d519435a09ac54e4bef117f6909da236b4793c2
Kun Yang <kunyangnew@gmail.com>
2017-08-25 00:01:07 -0700

    update

---
+++ application/views/mobile/contact.php
@@ -0,0 +1,11 @@
+<style>
+.cu-line{line-height:30px;font-size:16px}
+</style>
+<div data-role="page" id="resource-list" data-theme="d">
+	<?php $this->load->view('mobile/header', 'Contact Us');?>
+	<div data-role="main" class="ui-content" data-theme="d">
+	<div class="cu-line">Lily Zhu, Senior Marketing Director</div>
+	<div class="cu-line"> - Phone: (510) 461-3854</div>
+	<div class="cu-line"> - Email: lily_min_zhu@yahoo.com</div>
+	</div>
+</div>
