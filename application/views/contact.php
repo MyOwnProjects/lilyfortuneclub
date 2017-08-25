e5a181ad51c181c0b686ae53a5efb6cea15377c4
Kun Yang <kunyangnew@gmail.com>
2017-08-25 00:04:28 -0700

    update

--- application/views/contact.php
+++ application/views/contact.php
@@ -3,7 +3,7 @@
 </style>
 
 <div style="max-width:1000px;width:100%;padding:40px 20px;margin:0 auto">
-	<h3>Contact Us</h3>
+	<h3 class="text-center">Contact Us</h3>
 	<div class="cu-line">Lily Zhu, Senior Marketing Director</div>
 	<div class="cu-line"> - Phone: (510) 461-3854</div>
 	<div class="cu-line"> - Email: lily_min_zhu@yahoo.com</div>
--- application/views/mobile/contact.php
+++ application/views/mobile/contact.php
@@ -2,7 +2,7 @@
 .cu-line{line-height:30px;font-size:16px}
 </style>
 <div data-role="page" id="resource-list" data-theme="d">
-	<?php $this->load->view('mobile/header', 'Contact Us');?>
+	<?php $this->load->view('mobile/header', array('header_text' => 'Contact Us'));?>
 	<div data-role="main" class="ui-content" data-theme="d">
 	<div class="cu-line">Lily Zhu, Senior Marketing Director</div>
 	<div class="cu-line"> - Phone: (510) 461-3854</div>
