4400341b0c3c5accf628717e41ec52ae3825171e
Kun Yang <kunyangnew@gmail.com>
2017-08-24 23:52:31 -0700

    update

---
+++ application/views/contact.php
@@ -0,0 +1,9 @@
+<style>
+.cu-line{line-height:30px;font-size:16px}
+</style>
+
+<div style="max-width:1000px;width:100%;padding:40px 20px;margin:0 auto">
+	<h3>Contact Us</h3>
+	<div class="cu-line"> - Phone: (510) 461-3854</div>
+	<div class="cu-line"> - Email: lily_min_zhu@yahoo.com</div>
+</div>
