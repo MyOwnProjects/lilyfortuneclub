9d863558bbdedebb464cdc9952bd0127456c17c9
Kun Yang <kunyangnew@gmail.com>
2017-08-29 16:47:18 -0700

    update

--- application/views/mobile/contact.php
+++ application/views/mobile/contact.php
@@ -7,19 +7,19 @@
 	<?php $this->load->view('mobile/header', array('header_text' => 'Contact Us'));?>
 	<div data-role="main" class="ui-content" data-theme="d">
 		<div class="panel-heading">Senior Marketing Director</div>
-		<div class="panel-body">Lily Zhu<br/>(510) 461-3854<br/>lily_minzhu@yahoo.com</div>
+		<div class="panel-body"><u>Lily Zhu</u><br/>(510) 461-3854<br/>lily_minzhu@yahoo.com</div>
 		<div class="panel-heading">Invitation and Objection Handling</div>
-		<div class="panel-body">Lily Zhu<br/>(510) 501-4697<br/>lily_minzhu@yahoo.com</div>
-		<div class="panel-body">Sherry Hu<br/>(415) 264-1889<br/>Ms_huxin@hotmail.com</div>
-		<div class="panel-body">Minna Millare<br/>(415) 828-8841<br/>Minna.millare@gmail.com</div>
+		<div class="panel-body"><u>Lily Zhu</u><br/>(510) 501-4697<br/>lily_minzhu@yahoo.com</div>
+		<div class="panel-body"><u>Sherry Hu</u><br/>(415) 264-1889<br/>Ms_huxin@hotmail.com</div>
+		<div class="panel-body"><u>Minna Millare</u><br/>(415) 828-8841<br/>Minna.millare@gmail.com</div>
 		<div class="panel-heading">Products</div>
-		<div class="panel-body">Lily Zhu<br/>(510) 501-4697<br/>lily_minzhu@yahoo.com</div>
-		<div class="panel-body">Yiqing Du<br/>(510) 754-9984<br/>yiqingdu@hotmail.com</div>
-		<div class="panel-body">Bob Wang<br/>(408) 718-3622<br/>bob888wang@gmail.com</div>
+		<div class="panel-body"><u>Lily Zhu</u><br/>(510) 501-4697<br/>lily_minzhu@yahoo.com</div>
+		<div class="panel-body"><u>Yiqing Du</u><br/>(510) 754-9984<br/>yiqingdu@hotmail.com</div>
+		<div class="panel-body"><u>Bob Wang</u><br/>(408) 718-3622<br/>bob888wang@gmail.com</div>
 		<div class="panel-heading">License Procedure</div>
-		<div class="panel-body">Kun Yang<br/>(510) 461-3854<br/>kunyangnew@gmail.com</div>
-		<div class="panel-body">Christine Cen<br/>(510) 364-0502<br/>chrislicen@gmail.com</div>
+		<div class="panel-body"><u>Kun Yang</u><br/>(510) 461-3854<br/>kunyangnew@gmail.com</div>
+		<div class="panel-body"><u>Christine Cen</u><br/>(510) 364-0502<br/>chrislicen@gmail.com</div>
 		<div class="panel-heading">Name Tag, Team Shirt and Binder Order</div>
-		<div class="panel-body">Christine Cen<br/>(510) 364-0502<br/>chrislicen@gmail.com</div>
+		<div class="panel-body"><u>Christine Cen</u><br/>(510) 364-0502<br/>chrislicen@gmail.com</div>
 	</div>
 </div>
