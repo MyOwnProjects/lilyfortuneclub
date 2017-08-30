522edb8689beed26fea75d86e8e7d8eaf9029ba2
Kun Yang <kunyangnew@gmail.com>
2017-08-30 00:07:58 -0700

    update

--- application/views/mobile/seminar.php
+++ application/views/mobile/seminar.php
@@ -1,24 +1,97 @@
-<div data-role="page" id="event-list" data-theme="d">
+<?php
+$back_imgs = array(
+	'bkg_01_january.jpg','bkg_02_february.jpg','bkg_03_march.jpg', 'bkg_04_april.jpg', 'bkg_05_may.jpg', 'bkg_06_june.jpg',
+	'bkg_07_july.jpg','bkg_08_august.jpg','bkg_09_september.jpg', 'bkg_10_october.jpg', 'bkg_11_november.jpg', 'bkg_12_december.jpg'
+);
+$month = intval($month);
+?>
+<style>
+.background-list{position:relative;font-family:Comic Sans MS, cursive, sans-serif;font-size:16px;min-height:300px}
+.background-list:after{
+	content: "";
+	background-image:url('<?php echo base_url();?>src/img/12 Google Calendar Walls/<?php echo $back_imgs[$month - 1];?>');
+	background-size:100% 100%;
+	opacity: 0.2;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	position: absolute;
+	z-index: -1;
+}
+.background-list-block{padding:10px 0}
+.background-list-block>div{text-align:center;line-height:30px;}
+</style>
+<div data-role="page" id="seminar" data-theme="d">
 	<?php $this->load->view('mobile/header', array('header_text' => 'Seminar'));?>
 	<div data-role="main" class="ui-content" data-theme="d">
-		<form class="ui-filterable ui-content ui-mini ">
-			<input id="event-filter" data-type="search">
-		</form>
-		<ul data-role="listview" data-filter="true" data-input="#event-filter">
-			<?php
-			foreach($list as $l){
-			?>
-			<li class="clearfix">
-			<a href="src/schedule/<?php echo $l['schedule_year'].'/'.$l['file'];?>" target="_blank" class="nondisc" data-transition="slide" style='font-weight:normal'>
+			<div class="background-list">
+			<h3 style="margin-bottom:20px;text-align:center;font-family:Comic Sans MS, cursive, sans-serif !important;border-bottom:1px solid #d5d5d5"><?php echo date_format(date_create("$year-".($month)."-01"), 'F, Y');?></h3>
 				<?php
-				$text = $l['schedule_year'].'-'.$l['schedule_month'].', '.(empty($l['office_name']) ? 'Other' : $l['office_name']).', '.$l['file'];
-				echo $text;
+				if(count($list)== 0){
 				?>
-			</a>
-			</li>			
-			<?php
+				<div style="line-height:300px;text-align:center">No schedule available.</div>
+				<?php
+				}
+				foreach($list as $l){
+					$start_date = date_create($l['schedule_date_start']);
+					if(!empty($l['schedule_date_end'])){
+						$end_date = date_create($l['schedule_date_end']);
+					}
+					else{
+						$end_date = null;
+					}
+
+					$pos = strpos($l['file'], '.');
+					if($pos !== false){
+						$text = substr($l['file'], 0, $pos);
+					}
+					$text = str_replace('_', ' ', $text);
+				?>
+					<div class="background-list-block">
+						<div>
+						<?php echo date_format($start_date, 'M d, Y');
+							if(!empty($end_date)){
+							?>
+							 - 
+							<?php
+								echo date_format($end_date, 'M d, Y');
+							}
+							?>
+						</div>
+						<div>
+							<?php echo (empty($l['office_name']) ? 'Other' : $l['office_name']);?>
+						</div>
+						<div>
+								<a href="<?php echo base_url();?>src/schedule/<?php echo date_format($start_date, 'Y').'/'.$l['file'];?>" target="_blank"><?php echo $text;?></a>
+						</div>
+					</div>
+				<?php
+				}
+				?>
+			</div>
+			<?php 
+			if($month == 1){
+				$last_month = 12;
+				$last_year = $year - 1;
+			}
+			else{
+				$last_month = $month - 1;
+				$last_year = $year;
+			}
+			if($month == 12){
+				$next_month = 1;
+				$next_year = $year + 1;
+			}
+			else{
+				$next_month = $month + 1;
+				$next_year = $year;
 			}
 			?>
-		</ul>
+			
+			<div class="page-nav">
+				<a class="nav-prev" data-role="button" data-inline="true" data-mini="true" data-theme="b" data-icon="arrow-l" href="<?php echo base_url();?>seminar?year=<?php echo $last_year;?>&month=<?php echo $last_month;?>" data-transition="slide" data-iconpos="left" data-direction="reverse">Prev</a>
+				<a class="nav-next" data-role="button" data-icon="arrow-r" data-theme="b" href="<?php echo base_url();?>seminar?year=<?php echo $next_year;?>&month=<?php echo $next_month;?>" data-transition="slide" data-iconpos="right" data-inline="true" data-mini="true">Next</a>
+			</div>
 	</div>
 </div>
