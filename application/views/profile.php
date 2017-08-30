d25ded3e352d7e5ee1db3d24dae8047e4046f8d9
Kun Yang <kunyangnew@gmail.com>
2017-08-29 17:09:31 -0700

    update

--- application/views/profile.php
+++ application/views/profile.php
@@ -24,8 +24,24 @@
 				<li class="list-group-item list-group-item-info clearfix"><div><label>General Info</label></div></li>
 				<li class="list-group-item clearfix"><div><label>Name:</label></div><div><?php echo $user['first_name'].' '.$user['last_name'];?></div></li>
 				<li class="list-group-item clearfix"><div><label>Nick Name:</label></div><div><?php echo $user['nick_name'];?></div></li>
-				<li class="list-group-item clearfix"><div><label>Preference:</label></div><div class="btn-link" data-toggle="tooltip" title="Please contact your SDM, Lily Zhu, to change your preference."><?php echo $user['preference'];?></div></li>
-				<li class="list-group-item clearfix"><div><label>Email:</label></div><div><?php echo $user['email'];?></div></li>
+				<li class="list-group-item clearfix">
+					<div><label>Preference:</label></div>
+					<div>
+						<div class="radio" style="margin-top:0 !important;">
+							<label><input type="radio" name="preference" value="E" <?php echo $user['preference'] == 'E' ? 'checked' : '';?>>Learn knowledge</label>
+						</div>
+						<div class="radio">
+							<label><input type="radio" name="preference" value="B" <?php echo $user['preference'] == 'B' ? 'checked' : '';?>>Do business</label>
+						</div>
+						<div class="radio disabled">
+							<label><input type="radio" name="preference" value="BE" <?php echo $user['preference'] == 'BE' ? 'checked' : '';?>>Both</label>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item clearfix">
+					<div><label>Email:</label></div>
+					<div><?php echo $user['email'];?></div>
+				</li>
 				<li class="list-group-item clearfix"><div><label>Date of Birth:</label></div><div><?php echo date_format(date_create($user['date_of_birth']), 'F j');?></div></li>
 				<li class="list-group-item clearfix"><div><label>Phone:</label></div><div><?php echo $user['phone'];?></div></li>
 				<li class="list-group-item clearfix"><div><label>Address:</label></div><div>
@@ -39,3 +55,24 @@
 		</div>
 	</div>
 </div>
+<script>
+function update_user_preference(val){
+	ajax_loading(true);
+	$.ajax({
+		url: '<?php echo base_url();?>account/profile/update_preference',
+		method: 'post',
+		data: {preference:val},
+		success: function(){
+			
+		},
+		error: function(){
+		},
+		complete: function(){
+			ajax_loading(flse);
+		}
+	});
+}
+$('input[name=preference]').change(function(){
+	update_user_preference($(this).val());
+});
+</script>
