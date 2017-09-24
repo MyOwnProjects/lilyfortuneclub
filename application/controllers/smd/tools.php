b6164adf2ca7deff84870694d8c8409b7e1b4435
Kun Yang <kunyangnew@gmail.com>
2017-09-24 14:22:23 -0700

    udpate

--- application/controllers/smd/tools.php
+++ application/controllers/smd/tools.php
@@ -20,20 +20,17 @@ class Tools extends Smd_Controller {
 	public function direct_query(){
 		$sql = $this->input->post('sql');
 		$this->load->model('my_database_model');
+		$this->my_database_model->trans_begin();
 		try{
-			$this->my_database_model->trans_begin();
 			$result = $this->my_database_model->query_obj($sql);
-			$affected_rows = $this->my_database_model->affected_rows();
-			if($affected_rows >= 0){
-				throw new Exception('Query cannot change the content.');
-			}
-			$this->my_database_model->trans_commit();
+			$this->my_database_model->trans_rollback();
 			echo json_encode(array('success' => true, 'result' => $result));
 		}
 		catch(Exception $e){
 			$this->my_database_model->trans_rollback();
 			echo json_encode(array('success' => false, 'message' => $e->getMessage()));
 		}
+		$this->my_database_model->trans_rollback();
 	}
 	
 	public function elite_qualification(){
