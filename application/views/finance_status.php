55d90af283abedafa520f9d729ccc455517fcf81
Kun Yang <kunyangnew@gmail.com>
2017-09-27 22:35:33 -0700

    update

--- application/views/finance_status.php
+++ application/views/finance_status.php
@@ -16,6 +16,6 @@ iframe{width:100%;border:0}
 <div class="main-content-wrapper">
 	<h2 class="text-center">Know Your Family Finance Status</h2>
 	&nbsp;
-	<p><a class="text-link" href="'.base_url().'contact" target="_blank">Contact</a> yout SMD, Lily Zhu, to appoint a one-hour financial analysis.</p>
+	<p><a class="text-link" href="<?php echo base_url();?>contact" target="_blank">Contact</a> yout SMD, Lily Zhu, to appoint a one-hour financial analysis.</p>
 	<p>Lily Zhu: (510) 501-4697, lily_min_zhu@yahoo.com</p>
 </div>
