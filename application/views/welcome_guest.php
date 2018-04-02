ef79cd417d7821f93ef9c72e29fe0caaa882c0ab
Kun Yang <kunyangnew@gmail.com>
2018-04-01 22:47:35 -0700

    update

--- application/views/welcome_guest.php
+++ application/views/welcome_guest.php
@@ -3,15 +3,16 @@ p{line-height:30px;font-size:16px}
 div.img>div{float:left;margin:5px 2px;height:290px;width:135px;text-align:center;}
 div.img img{height:180px;width:135px;}
 div.img>div>div{margin:5px 0}
-ul{margin-left:30px !important}
+ul{margin-left:30px !important;line-height:30px;font-size:16px;}
 li{list-style:square !important;}
+li>div{font-size:14px;padding-left:10px}
 </style>
 <div class="main-content-wrapper">
 	<h2 class="text-center">Welcome to Lily Fortune Club</h2>
 	<br/>
 	<p>Dear New Member,</p>
 	<p>Welcome to our team and our extended business family!</p>
-	<p><img style="float:left;margin-right:20px;width:250px" src="<?php echo base_url();?>src/img/h-TRANSAMERICA-PYRAMID-PHOTOS-628x314.jpg">We are under the big umbrella of Transamerica, who has been helping people feel better about the future for more than 100 years, and we’re proud of the trust we’ve earned. To learn more, please visit our website at <a href="http://www.transamerica.com" target="_blank">http://www.transamerica.com</a></p>
+	<p><img style="float:left;margin-right:20px;width:250px" src="<?php echo base_url();?>src/img/h-TRANSAMERICA-PYRAMID-PHOTOS-628x314.jpg">We are under the big umbrella of Transamerica, who has been helping people feel better about the future for more than 100 years, and we’re proud of the trust we’ve earned. To learn more, please visit our website at <a href="http://www.transamerica.com" target="_blank">www.transamerica.com</a></p>
 	<br/>
 	<p>Congratulations on joining a winning team, led by influential business and community leaders such as:</p>
 	<div class="img clearfix">
@@ -45,7 +46,12 @@ li{list-style:square !important;}
 		<li><a href="<?php echo base_url();?>account/documents/view/5ac1b8002b7d7" target="_blank">Financial Concept Classes</a></li>
 		<li><a href="<?php echo base_url();?>account/documents/view/5ac1b8002b7d7" target="_blank">Business and Marketing Classes</a></li>
 		<li><a href="<?php echo base_url();?>account/license" target="_blank">Financial Licensing Procedure</a></li>
-		<li>Financial Knowledge Resources</li>
+		<li>Financial Knowledge Resources
+			<div>-&nbsp;<a href="<?php echo base_url();?>resource/item/fa35e192121eabf3dabf9f5ea6abdbcbc107ac3b14" target="_blank">Lily Min Zhu's speech in 2017 Las Vegas Convention</a></div>
+			<div>-&nbsp;<a href="<?php echo base_url();?>resource/item/91032ad7bbcb6cf72875e8e8207dcfba80173f7c20" target="_blank">Indexed Universal Life: It's Just a Great Investment</a></div>
+			<div>-&nbsp;<a href="<?php echo base_url();?>resource/item/b3f0c7f6bb763af1be91d9e74eabfeb199dc1f1f19" target="_blank">Why Indexed Universal Life Insurance Can Be a Good Retirement Investment</a></div>
+			<div>-&nbsp;<a href="<?php echo base_url();?>resource/item/b7eb6c689c037217079766fdb77c3bac3e51cb4c51" target="_blank">Can I Really Have a Tax-Free Retirement?</a></div>
+		</li>
 	</ul>
 	<br/>
 	<p>
