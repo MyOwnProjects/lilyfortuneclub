830e8bb4b5c0ecbf42ce7e48f2df6d4670e548d2
Kun Yang <kunyangnew@gmail.com>
2018-09-27 23:02:26 -0700

    update

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,5 @@
 <footer>
 	<section><span>Add Favorite</span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<span>Terms of Service</span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<span>Privacy Policy</span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<span><a href="<?php echo base_url();?>contact">Contact Us</a></span></section>
 	<section>Copyright © 2016 lilyfortuneclub. All rights reserved.</section>
+<script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
 </footer>
