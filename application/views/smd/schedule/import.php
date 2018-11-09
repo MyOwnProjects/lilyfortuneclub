971fc28ef291ba1b1706ca27bd1b08170642f563
Kun Yang <kunyangnew@gmail.com>
2018-11-08 16:10:12 -0800

    update

--- application/views/smd/schedule/import.php
+++ application/views/smd/schedule/import.php
@@ -1,6 +1,13 @@
-<form method="post">
+<form method="post" class="form-inline">
 	<div style="padding:10px">
 		<input class="btn btn-primary btn-sm" type="submit" value="Submit">
+		<br/><br/>
+		<select class="form-control form-control-sm schedule_location_all">
+			<option value="Fremont">Fremont</option>
+			<option value="San Jose">San Jose</option>
+			<option value="Pleasanton">Pleasanton</option>
+			<option value="0">Other</option>
+		</select>
 	</div>
 	<table cellspacing="0" cellpadding="0">
 		<thead>
@@ -13,7 +20,7 @@
 			<td><?php echo $i + 1;?></td>
 			<td style="width:225px">
 				<div class="input-group">
-					<input class="form-control form-control-sm" type="date" name="schedule_start_date[]" style="width:125px">
+					<input class="form-control form-control-sm schedule_start_date" type="date" name="schedule_start_date[]" style="width:125px">
 					<select class="form-control form-control-sm schedule_start_time" name="schedule_start_time[]" style="width:55px">
 						<option value="0">None</option>
 						<?php
@@ -46,7 +53,7 @@
 			<td><input class="form-control form-control-sm" name="schedule_topic[]" style="width:150px"></td>
 			<td><input class="form-control form-control-sm" name="schedule_presenters[]" style="width:100px"></td>
 			<td>
-				<select class="form-control form-control-sm" name="schedule_location[]">
+				<select class="form-control form-control-sm schedule_location" name="schedule_location[]">
 					<option value="Fremont">FR</option>
 					<option value="San Jose">SJ</option>
 					<option value="Pleasanton">PL</option>
@@ -62,6 +69,20 @@
 	</table>
 </form>
 <script>
+$('.schedule_location_all').change(function(){
+	$('.schedule_location').val($(this).val());
+});
+	
+$('.schedule_start_date').change(function(){
+	var d = new Date($(this).val()).getDay();
+	if(d == 1 || d == 2){//Tue, Wed
+		$(this).next().val('19:00').trigger('change');
+	}
+	else if(d == 0 || d == 6){//Mon, Sun
+		$(this).next().val('10:00').trigger('change');
+	}
+	
+});
 $('.schedule_start_time').change(function(){
 	if($(this).val() == 0){
 		$(this).parent().parent().next().find('.schedule_end_time').val(0);
