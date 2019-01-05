346c80bd37af89de5f8261ee1465c521b37ea25b
Kun Yang <kunyangnew@gmail.com>
2019-01-05 00:20:35 -0800

    update

--- application/views/about.php
+++ application/views/about.php
@@ -6,11 +6,9 @@
 <div style="max-width:1000px;width:100%;padding:40px 20px;margin:0 auto">
 	<h3 class="text-center">About Us</h3>
 	<div style="margin:40px auto;width:100%">
-	<p>We are financial doctors; we are financial brokers.</p>
-	<p>We help provide solutions; we are client oriented vs. company oriented.</p>
-	<p>We teach financial knowledge, leadership skills, and provide field training. </p>
-	<p>Built in 2013, Lily Fortune Club, has established a well respected professional brand, especially among highly educated high-tech professional customers. We are also dedicated to establish the US asset diversification channel for oversea high net worth individuals and to provide long term and dependable professional services. We are passionate about helping people and educating local communities, and actively engaged in nonprofit and entrepreneur organizations.</p>
-	<p>The founder, Lily Zhu, is ranked as 2016 top 25 (0.1%) out of ~70,000 financial professionals and was recently recognized in the US Fortune magazine in 2017. Lily and her team represent over 200 top rated financial companies to provide total solutions and customized strategies include investment, retirement, life insurance, long term care, tax planning, estate planning and education planning.</p>
-	<p>Join us, be CEO of your own Business!</p>
+	<p>Lily Fortune Club has established a well respected professional brand, as a financial broker, in the Silicon Valley, California. We have operations in CA, WA, NY, NJ, MA, PA and Vancouver Canada. The founder, Lily Zhu, possesses a unique combination of entrepreneur, finance business and corporate management experience over 18 years. Lily was ranked as top 25 (0.1%) out of ~70,000 financial professionals in 2016 and was recognized in the US Fortune magazine in 2017.</p>
+	<p>Lily and her team represent over 200 top rated financial companies to provide total solutions and customized strategies including investment, retirement, life insurance, long term care, tax planning, estate planning and education planning.</p>
+	<p>We are also dedicated to establish the US asset diversification channel for oversea high net worth individuals and to provide long term and dependable professional services. We are passionate about helping people and educating local communities, and actively engaged in nonprofit and entrepreneur organizations.</p>
+	<p>We are currently expanding to serve more markets, and looking for potential business partners in US, Canada, and foreign countries such as China and India. Please contact us for details. Join us, be the CEO of your own business!</p>
 	</div>
 </div>
