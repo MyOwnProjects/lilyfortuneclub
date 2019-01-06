6d2ad54ac9c904bdca202438490de9972d65bf3f
Kun Yang <kunyangnew@gmail.com>
2019-01-06 11:04:47 -0800

    update

--- application/views/privacy.php
+++ application/views/privacy.php
@@ -8,12 +8,16 @@
 	<div style="margin:40px auto;width:100%;font-size:16px">
 	<h4>Legal Information & Website Use</h4>
 	<p>View our legal terms and information regarding the use of our website.</p>
+	<br/>
 	<h4>Our Privacy Promise</h4>
 	<p>Read about our efforts to protect your information.</p>
+	<br/>
 	<h4>Online Privacy Policy</h4>
 	<p>Information about our policies on protecting your privacy online.</p>
+	<br/>
 	<h4>Help Us Protect Your Privacy</h4>
 	<p>Read our tips to protect yourself from phishing and other online scams.</p>
+	<br/>
 	<h4>Our Social Media Policy</h4>
 	<p>View our guidelines for our social media sites.</p>
 	</div>
