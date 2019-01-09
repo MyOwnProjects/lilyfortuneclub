43282e8ce155eebd02749f1050b0decc882f751d
Kun Yang <kunyangnew@gmail.com>
2019-01-09 15:10:05 -0800

    update

--- application/views/account/welcome_email.php
+++ application/views/account/welcome_email.php
@@ -19,24 +19,23 @@
 <div style="margin:0 auto;max-width:1000px;padding:20px 0 80px 0;">
 	<h2 class="text-center">Welcome Email</h2>
 		<div class="form-group">
-			<label>Select the member</label>
+			<label>Select the member <span style="font-weight:normal !important">(Enter the name or code to select)</span></label>
 			<select class="form-control control-sm" data-live-search='true' id="member-select">
-				<option value="<?php echo $user['membership_code'];?>"><?php echo $user['first_name'].' '.$user['last_name'].' ('.$user['membership_code'].')';?></option>
 				<?php 
 				foreach($members as $m){
 				?>
-				<option value="<?php echo $m['membership_code'];?>"><?php echo $m['name'];?></option>
+				<option value="<?php echo $m['membership_code'];?>"><?php echo $m['name'].' - '.$m['membership_code'];?></option>
 				<?php 
 				}
 				?>
 			</select>
-			<br/>
-			<button class="btn btn-primary" onclick="get_member_info();">Select</button>
+			
+			<button style="margin:10px 0 20px 0" class="btn btn-success" onclick="get_member_info();">Select</button>
 		</div>
-		<textarea id="email-template" style="width:100%"></textarea>	
+		<textarea id="email-template" style="width:100%;height:100px"></textarea>	
 </div>
 <script>
-$('#recruits-baseshop-select').selectpicker();
+$('#member-select').selectpicker();
 function AutoGrowTextArea(textField)
 {
   if (textField.clientHeight < textField.scrollHeight)
@@ -59,7 +58,7 @@ function get_member_info(){
 			var sep = "\n";
 			var welcome_email_template = "Hi "+ (data['info']['nick_name'] ? data['info']['nick_name'] : data['info']['first_name']) + ","
 				+ sep
-				+ sep + "Hearty Congratulations on becoming a member of Lily Fortune Club/WFG!"
+				+ sep + "Hearty Congratulations on becoming a member of Lily Fortune Club / WFG!"
 				+ sep
 				+ sep + "Your associate ID is " + data['info']['membership_code'] 
 				+ sep
@@ -74,7 +73,7 @@ function get_member_info(){
 				+ sep + "Your account on lilyfortuneclub.com is already set up. Go to https://www.lilyfortuneclub.com/ac/sign_in. Your username is your Associate ID, and your password is Associate ID followed by your last name, all capital. After you successfully log in, you can review and use the source on it."
 				+ sep
 				+ sep + "I strongly encourage you to do the following two things:"
-				+ sep + "- Review your personal financial plan. Secure your family, retirement and future. There is no need to buy anything or spend money but let one of the experts discuss your financial planning and explain the gaps. Your continuous learning will help to validate the facts/findings. I recommend to do this within one week to get started your journey, we will engage the experienced advisers from Lily Fortune Club/WFG."
+				+ sep + "- Review your personal financial plan. Secure your family, retirement and future. There is no need to buy anything or spend money but let one of the experts discuss your financial planning and explain the gaps. Your continuous learning will help to validate the facts / findings. I recommend to do this within one week to get started your journey, we will engage the experienced advisers from Lily Fortune Club / WFG."
 				+ sep + "- Get Licensed as Insurance Agent and then as a financial adviser."
 				+ sep
 				+ sep + "To get the license, go to https://www.lilyfortuneclub.com/license, and follow the instruction."
@@ -83,7 +82,8 @@ function get_member_info(){
 				+ sep
 				+ sep + "Please contact me directly if you have any questions on how to go forward."
 				+ sep
-				+ sep + "Best regards,";
+				+ sep + "Best regards,"
+				+ sep + sep + sep;
 			$('#email-template').html(welcome_email_template);
 			AutoGrowTextArea($('#email-template')[0]);
 		},
