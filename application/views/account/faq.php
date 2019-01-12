6714a6793957583f8cd64e76fd26d3986429f1be
Kun Yang <kunyangnew@gmail.com>
2019-01-11 19:12:37 -0800

    update

--- application/views/account/faq.php
+++ application/views/account/faq.php
@@ -53,7 +53,7 @@
 					foreach($sub_category['questions'] as $q_id => $c){
 					?>
 						<div class="col-lg-6 col-md-6 col-sm-12 clearfix" style="padding:10px">
-							<div class="pull-left"><?php echo $i++;?>. </div> 
+							<div class="pull-left" style="width:30px;color:#337ab7"><?php echo $i++;?>. </div> 
 							<div style="overflow:hidden"><a href="#<?php echo $q_id;?>"><?php echo $c['subject'];?></a></div>
 						</div>
 					<?php
