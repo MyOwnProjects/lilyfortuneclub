44c8c4e9047e392fae273cbbaa2b9cae0d473267
Kun Yang <kunyangnew@gmail.com>
2019-10-01 20:43:46 -0700

    update

--- application/controllers/training.php
+++ application/controllers/training.php
@@ -57,7 +57,7 @@ class Training extends Base_Controller {
 		$file_info = pathinfo ($full_file);
 		$ext = strtolower($file_info['extension']);
 		if($ext == 'pdf'){
-			$this->load->view('pdf_viewer_1', array('file' => $file, 'name' => $file_info['basename']));
+			$this->load->view('pdf_viewer_1', array('file' => $file.'&'.time(), 'name' => $file_info['basename']));
 			//$this->load->view('pdf_viewer', array('file' => $file));
 		}
 		else if($ext == 'mp4'){
