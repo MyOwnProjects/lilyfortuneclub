648f32948f286b35e9069ec23b5e8c3cff11bd30
Kun Yang <kunyangnew@gmail.com>
2019-11-01 23:16:44 -0700

    Update list.php

--- application/views/smd/tasks/list.php
+++ application/views/smd/tasks/list.php
@@ -5,7 +5,7 @@
 	$('#tasks-grid').data_table({
 		header: [
 			{id: 'tasks_case_no', text: 'Case #', sortable: true, width:'80px', align: 'left'}, 
-			{id: 'tasks_subject', text: 'Subject',narrow_display: true}, 
+			{id: 'tasks_subject', text: 'Subject',narrow_display: true, sortable: true}, 
 			{id: 'tasks_type', text: 'Type', width:'100px', align: 'center', sortable: true}, 
 			{id: 'tasks_priority', text: 'Priority', sortable: true, width:'60px', align: 'center',narrow_display: true}, 
 			//{id: 'tasks_create', text: 'Create', sortable: true, width:'50px', align:'center'},
