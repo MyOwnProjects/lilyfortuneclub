b63e8cf5f0e6387ca721b14eb992a5faacec1d62
Kun Yang <kunyangnew@gmail.com>
2020-11-22 11:39:05 -0800

    update

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,5 @@
 <footer>
 <div><a href="<?php echo base_url();?>legal_notice">Terms of Service</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url();?>privacy">Privacy Policy</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url();?>contact">Contact Us</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url();?>about">About Us</a></div>
-<div>Copyright © 2016 Lily Fortune Club.</div>
+<div>Copyright © 2020 Lily Fortune Club.</div>
 <script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
 </footer>
