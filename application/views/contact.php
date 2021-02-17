9cb9060a3eeb62383f8c8481fbbe6c07a9d11a52
Kun Yang <kunyangnew@gmail.com>
2021-02-16 17:04:58 -0800

    update

--- application/views/contact.php
+++ application/views/contact.php
@@ -11,7 +11,16 @@
 		?>
 		<div class="panel panel-success">
 			<div class="panel-heading">Senior Marketing Director</div>
-			<div class="panel-body"><b>Lily Zhu</b> (510) 501-4697, lily_min_zhu@yahoo.com</div>
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-sm-4 col-xs-6">
+						<b>Lily Zhu</b><br/>
+						(510) 501-4697<br/>
+						lily_min_zhu@yahoo.com<br/>
+						<a HREF="https://calendly.com/lilyminzhu" target="_blank">Calendly</a>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="panel panel-info">
 				<div class="panel-heading">Invitation and Objection Handling</div>
@@ -22,7 +31,7 @@
 							(510) 501-4697<br/>
 							lily_min_zhu@yahoo.com
 						</div>
-						<div class="col-sm-4 col-xs-6">
+						<!--div class="col-sm-4 col-xs-6">
 							<b>Minna Millare</b><br/>
 							(415) 828-8841<br/>
 							Minna.millare@gmail.com
@@ -31,7 +40,7 @@
 							<b>Bob Wang</b><br/>
 							(408) 718-3622<br/>
 							bob888wang@gmail.com
-						</div>
+						</div-->
 					</div>
 				</div>
 			</div>
@@ -64,11 +73,11 @@
 							(510) 461-3854<br/>
 							kunyangnew@gmail.com
 						</div>
-						<div class="col-xs-6">
+						<!--div class="col-xs-6">
 							<b>Christine Cen</b><br/>
 							(510) 364-0502<br/>
 							chrislicen@gmail.com
-						</div>
+						</div-->
 					</div>
 				</div>
 			</div>
